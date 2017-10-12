2cbbb376713539fef6db57c194576d75aaf07e62
Ben <bgaramy@hotmail.com>
2017-10-12 13:52:38 +1000

    Optimised

--- imagegen.py
+++ imagegen.py
@@ -6,7 +6,7 @@ from random import randint
 
 point = namedtuple("Point", "target current distance")
 min_width = 2
-max_width = 50
+max_width = 40
 BASE_COLOR = (255,255,255)
 img_w = 0
 img_h = 0
@@ -21,6 +21,7 @@ def get_current_data(points):
 			data.append(points[h][w].current)
 	return data
 
+
 im = Image.open("squirrel.jpg")
 imdata = im.getdata()
 colours = list({c for c in imdata})
@@ -34,6 +35,10 @@ for h in range(im.size[1]):
 		target = data_gen.next()
 		progress[h].append(point(target, BASE_COLOR, get_distance(BASE_COLOR, target)))
 
+successes = 0
+out = 0
+new = Image.new(mode="RGB", size=im.size, color=BASE_COLOR)
+
 for i in range(1000000):
 	origin = (randint(0, im.size[0]-1), randint(0, im.size[1]-1))
 	size = randint(min_width,max_width)
@@ -41,28 +46,36 @@ for i in range(1000000):
 
 	#current_avg
 	#new_avg
-	distances = []
-	for h in range(size):
-		for w in range(size):
-			if h+origin[1]<im.size[1] and w+origin[0]<im.size[0]:
-				distances.append(get_distance(colour, progress[h+origin[1]][w+origin[0]].target))
-	avg_distance = sum(distances)/len(distances)
 
+	sum_distances = 0
+	distances = size**2
+	new_distances = []
 	cur_distances = []
 	for h in range(size):
+		new_distances.append([])
 		for w in range(size):
 			if h+origin[1]<im.size[1] and w+origin[0]<im.size[0]:
-				cur_distances.append(get_distance(progress[h+origin[1]][w+origin[0]].current, progress[h+origin[1]][w+origin[0]].target))
-	cur_distance = sum(cur_distances)/len(cur_distances)
+				new_distance = get_distance(colour, progress[h+origin[1]][w+origin[0]].target)
+				new_distances[h].append(new_distance)
+				sum_distances += new_distance
+				cur_distances.append(progress[h+origin[1]][w+origin[0]].distance)
+	avg_distance = sum_distances/distances				
+	cur_distance = sum(cur_distances)/distances
 
 	if avg_distance < cur_distance:
 		for h in range(size):
 			for w in range(size):
 				if h+origin[1]<im.size[1] and w+origin[0]<im.size[0]:
-					progress[h+origin[1]][w+origin[0]] = point(progress[h+origin[1]][w+origin[0]].target, colour, 0)
-		new = Image.new(mode="RGB", size=im.size, color=(0,0,0))
-		new.putdata(get_current_data(progress))
-		new.save("squirrel{}.jpg".format(i))
+					progress[h+origin[1]][w+origin[0]] = point(progress[h+origin[1]][w+origin[0]].target, colour, new_distances[h][w])
+		
+
+		paste = Image.new(mode="RGB", size=(size,size), color=colour)
+		new.paste(paste, origin)
+		successes += 1
+
+		if successes%16==0:
+			out+=1
+			new.save("output/squirrel{}.jpg".format(out))
 
 
 #for each pixel, store target, current and distance
